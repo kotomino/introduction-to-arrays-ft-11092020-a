962a6a2a385ff84c68ea14c6405a8507eaa3e25c
Kotomi Noguchi <kotomi.noguchi@gmail.com>
2020-11-11 11:58:26 -0500

    added array code to arrays.rb

--- arrays.rb
+++ arrays.rb
@@ -3,7 +3,20 @@ require "pry"
 array = []
 name = "Bob"
 
-array = array.push(name)
+array.push(name)
 
+sarah = "Sarah"
+
+array.unshift(sarah)
+
+array.push("Tim")
+array.push("Lisa")
+
+removed_person = array.shift
+
+array.pop
+
+array.unshift(sarah)
+array.push("Lisa")
 
 binding.pry
